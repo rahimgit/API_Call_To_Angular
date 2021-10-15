12fd0760c7efb964020c9f80a743fbe82b2bdc18
Mushtaq Rahim <rahimt3@gmail.com>
2021-10-16 01:50:07 +0530

    Add project files.

---
+++ WebApplication5/Controllers/DepartmentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration) {
+
+            _configuration = configuration;
+        
+        
+        }
+
+        [HttpGet]
+        public JsonResult Get() {
+
+            string query = @"
+                          SELECT DepartmentId, DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader dr;
+            using (SqlConnection conn = new SqlConnection(sqlDataSource)) {
+
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn)) {
+
+                    dr = cmd.ExecuteReader();
+                    table.Load(dr);
+                    dr.Close();
+                    conn.Close();
+                
+                }
+            
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ WebApplication5/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoin { get; set; }
+      
+        public string PhotoFileName { get; set; }
+
+        
+    }
+}
